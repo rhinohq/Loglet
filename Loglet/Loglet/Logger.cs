39d19d5654458d797619bb3d654728c92d9d5dad
Ryan Kendrick <ryan@kendrickfamily.info>
2019-09-06 23:23:25 +0100

    Init Repo Upload

---
+++ Loglet/Loglet/Logger.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Loglet
+{
+    public class Logger : ExceptionLogger, IDisposable
+    {
+        private static StreamWriter Writer;
+
+        public Logger()
+        {
+            try
+            {
+                Directory.CreateDirectory("C:/Apollo Asset Manager/Asset Manager Logs");
+                Writer = new StreamWriter($"C:/Apollo Asset Manager/Asset Manager Logs/Log_{DateTime.Now.ToString("dd-MM-yyyy")}.txt", true);
+
+                Writer.WriteLine();
+                Writer.WriteLine($"<<<<<<<<<< Asset Manager Log {DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss")} >>>>>>>>>>");
+                Writer.WriteLine();
+            }
+            catch (Exception e)
+            {
+                Writer = StreamWriter.Null;
+                Out("Error creating log file: " + e.Message);
+            }
+        }
+
+        ~Logger()
+        {
+            Dispose(false);
+        }
+
+        public void UpdateWriter()
+        {
+            Writer = new StreamWriter($"C:/Apollo Asset Manager/Asset Manager Logs/Log_{DateTime.Now.ToString("dd-MM-yyyy")}.txt", true);
+
+            Writer.WriteLine();
+            Writer.WriteLine($"<<<<<<<<<< Asset Manager Log {DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss")} >>>>>>>>>>");
+            Writer.WriteLine();
+        }
+
+        public void Out(string str = "")
+        {
+            Trace.WriteLine(str);
+
+            if (str == "")
+                Writer.WriteLine(str);
+            else
+                Writer.WriteLine(string.Format("{0}  {1}", DateTime.Now.ToString("HH:mm:ss:fff"), str));
+
+            Writer.Flush();
+        }
+
+        public void Out(object obj)
+        {
+            Out(JsonConvert.SerializeObject(obj));
+        }
+
+        public void Error(string str)
+        {
+            Out("ERROR: " + str);
+        }
+
+        public void Error(string message, string stacktrace)
+        {
+            string str = $"ERROR: Message: {message} Stack Trace: {stacktrace}";
+
+            Out(str);
+        }
+
+        public void Error(Exception ex)
+        {
+            string str = $"ERROR: Message: {ex.Message} Method: {ex.TargetSite.Name}";
+
+            Out(str);
+            Out(ex.StackTrace);
+        }
+
+        public override void Log(ExceptionLoggerContext Context)
+        {
+            //Error(Context.ExceptionContext.Exception.Message, Context.ExceptionContext.Exception.TargetSite.Name);
+            Error(Context.ExceptionContext.Exception);
+        }
+
+        #region IDisposable Support
+
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    Writer.Flush();
+                    Writer.Close();
+
+                    Writer = StreamWriter.Null;
+
+                    //Writer.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        #endregion IDisposable Support
+    }
+}
---
+++ Loglet/Loglet/MidnightNotifier.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Timers;
+
+namespace Loglet
+{
+    public static class MidnightNotifier
+    {
+        private static readonly Timer timer;
+
+        static MidnightNotifier()
+        {
+            timer = new Timer(GetSleepTime());
+
+            timer.Elapsed += (s, e) =>
+            {
+                OnDayChanged();
+                timer.Interval = GetSleepTime();
+            };
+
+            timer.Start();
+
+#if NETFRAMEWORK
+            Microsoft.Win32.SystemEvents.TimeChanged += OnSystemTimeChanged;
+#endif
+        }
+
+        private static double GetSleepTime()
+        {
+            DateTime midnightTonight = DateTime.Today.AddDays(1);
+            double differenceInMilliseconds = (midnightTonight - DateTime.Now).TotalMilliseconds;
+
+            return differenceInMilliseconds;
+        }
+
+        private static void OnDayChanged()
+        {
+            DayChanged?.Invoke(null, null);
+        }
+
+        private static void OnSystemTimeChanged(object sender, EventArgs e)
+        {
+            timer.Interval = GetSleepTime();
+            Log.UpdateWriter();
+        }
+
+        public static event EventHandler<EventArgs> DayChanged;
+    }
+}
